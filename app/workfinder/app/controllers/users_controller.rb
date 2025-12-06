5a90eadef37af6461138030cbe2d0992d409fad6
Spencer Clarke <svampis@thinkcentre.thinkcenter.ddns.me>
2025-12-06 14:53:47 -0500

    added search by city

--- app/workfinder/app/controllers/users_controller.rb
+++ app/workfinder/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:city].present?
+      @users = User.where("city LIKE ?", "%" + params[:city] + "%")
+    else
+      @users = User.all
+    end
   end
 
   def show
